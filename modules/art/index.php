1f145847123b58cd8831f50c8bc5d65548895a78
resul92 <resulrza@gmail.com>
2017-04-28 21:31:43 +0400

    links appended

---
+++ modules/art/index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!--
+        Designed by R@sul Rz@yev
+        wwww.resulrzayev.com  www.resulrzayev.blogspot.com
+      -->
+
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+      <meta name="theme-color" content="#3F51B5">
+      <meta name="author" content="R@sul Rz@yev">
+      <meta name="description" content="Resul Rzayev personal webiste">
+      <meta name="keywords" content="Resul Rzayev,Programming tutorials,Software and Web Developer">
+
+      <link rel="icon" href="../../assets/media/images/favicon.png" type="image/x-icon">
+      <link rel="shortcut icon" href="assets/media/images/favicon.png" type="image/x-icon">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../../assets/styles/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="../../assets/styles/css/art.css" rel="stylesheet">
+     <link href="../../assets/styles/css/timeline.css" rel="stylesheet">
+    <link href="../../assets/styles/css/footer.css" rel="stylesheet">
+    <!-- FONT AWSIME -->
+    <link rel="stylesheet" type="text/css" href="../../assets/styles/css/font-awesome.min.css">
+     <script src="https://use.fontawesome.com/852fd2a7d5.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="../../assets/styles/css/animate.min.css">
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="http://rzaeeff.me">RESUL RZA<span style="color:#E7C95B">EEFF</span></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="http://rzaeeff.me/modules/portfolio/">Portfolio</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="http://rzaeeff.me/modules/tutorials/">TUTORIALS</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="http://rzaeeff.me/modules/art/">ART</a>
+                    </li>
+                     <li class="page-scroll">
+                        <a href="http://rzaeeff.me/modules/photography/">PHOTOGRAPHY</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header id="portfolio-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="../../assets/media/images/logo_blue.png" alt="" width="200">
+                    <div class="intro-text">
+                        <span class="name">RESUL RZA<span style="color:#2C3E50">EEFF</span></span>
+                        <hr class="star-light">
+                        <span class="skills">I am a full-stack developer with 5+ years experience in building web and desktop based applications. I have knowledge in both backend and frontend technologies.During my work experience I have developed different projects. My experience is mainly focused on Java frameworks.I have also have 2+ years of experience as .NET developer. Also, I have utilized my web design and creativity skills to develop websites with the help of technologies like HTML5, CSS3, JavaScript, jQuery, AngularJs,Bootstrap and PHP. Apart from these I have experience in desktop/web GIS application using OpenLayers and ArcGIS engine.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Image Gallery -->
+   
+
+    <!-- Portfolio Grid Section -->
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Gallery</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+               <div class="grid">
+
+                  <div class="grid-item"><img src="../../assets/media/images/art/vendetta.jpg" class="img-responsive" alt=""></div>
+                  <div class="grid-item"><img src="../../assets/media/images/art/wolfwoman.jpg" class="img-responsive" alt="">
+                      <pre>asdasdasdasdasd asdsadsadas
+                     asdasdasd asdasdas
+                     sadsadsada</pre>
+                  </div>
+                  <div class="grid-item " ><img src="../../assets/media/images/art/ferrari.jpg" class="img-responsive" alt="">
+                       <pre>asdasdasdasdasd asdsadsadas
+                     asdasdasd asdasdas
+                     sadsadsada</pre>
+                  </div>
+                  <div class="grid-item"><img src="../../assets/media/images/art/rain.jpg" class="img-responsive" alt=""></div>
+                  <div class="grid-item "><img src="../../assets/media/images/art/eye.jpg" class="img-responsive" alt=""></div>
+                  <div class="grid-item"><img src="../../assets/media/images/art/developer.jpg" class="img-responsive" alt=""></div>
+                  <div class="grid-item"><img src="../../assets/media/images/art/deer.jpg" class="img-responsive" alt=""></div>
+                  <div class="grid-item"><img src="../../assets/media/images/art/bird.jpg" class="img-responsive" alt=""></div>
+               </div>
+            </div>
+    </section>
+
+
+   
+    <!-- Footer -->
+     <!-- <?php  include('modules/footer.html'); ?> -->
+        <footer class="footer"> 
+       <div class="footer-social-icons">
+          <ul class="social-icons">
+              <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+              <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+              <li><a href="" class="social-icon"> <i class="fa fa-rss"></i></a></li>
+              <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
+              <li><a href="" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
+              <li><a href="" class="social-icon"> <i class="fa fa-google-plus"></i></a></li>
+          </ul>
+      </div>
+       <div class="footer_content">
+           <div class="left_side">
+                 <pre>
+                   Please feel free to contact me if
+                   you have any further questions 
+                   or concerns :
+
+
+                    <i class="fa fa-map-marker"></i>  <b>Azerbaijan,BAKU</b>
+                    <i class="fa fa-phone"></i> <b>(+994) 077-219-65-61</b>
+                    <i class="fa fa-envelope-open"></i> <b>resulrza@gmail.com</b>
+
+                 </pre>
+           </div>
+           <div class="line"></div>
+           <div class="right_side">
+                    <div class="group">
+                      <input type="text" required="required"/><span class="highlight"></span><span class="bar"></span>
+                      <label>Email</label>
+                    </div>
+                    <div class="group">
+                      <textarea type="textarea" rows="5" required="required"></textarea><span class="highlight"></span><span class="bar"></span>
+                      <label>Message</label>
+                    </div>
+                    <div class="buttons">
+                        <a href="#" class="button mail"><span>Send Mail</span></a>
+                    </div>
+           </div>
+       </div>
+       <div class="right">
+         <p class="copyright">
+            © 2017 Resul RzaEEFF - All rights reserved <span class="lang" key="copy"></span>
+         </p>
+       </div>
+
+       
+    </footer>
+
+
+    <!-- jQuery -->
+    <script src="../../assets/scripts/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../../assets/scripts/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Massonary Scripts -->
+    <script src="../../assets/scripts/js/masonry.pkgd.js"></script>
+    <script src="../../assets/scripts/js/masonry.pkgd.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="../../assets/scripts/js/portfolio.min.js"></script>
+
+  <script>
+  $('.grid').masonry({
+      itemSelector: '.grid-item'
+    });
+  </script>
+
+</body>
+
+</html>
